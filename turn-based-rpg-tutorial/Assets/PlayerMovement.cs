c688c520466ab22980fecb3b4e57a714ae37b8ac
thefrostappleyt <65557811+thefrostappleyt@users.noreply.github.com>
2021-04-14 18:46:56 -0400

    part 1
    part 1

---
+++ turn-based-rpg-tutorial/Assets/PlayerMovement.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float movementSpeed;
+    Rigidbody2D rigidbody2D;
+    Vector3 movement;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody2D = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        movement = Vector3.zero;
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+        Move();
+    }
+
+    void Move()
+    {
+        rigidbody2D.MovePosition(transform.position + movement * movementSpeed * Time.deltaTime);
+    }
+}
+
+
